104764bf5406c980d1e39bf72b3f8b9d40251a86
Phillip Henslee <ph2@ph2.us>
2015-11-02 16:47:56 -0600

    commit

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 var config = module.exports = {};
 
-config.token = 'xoxb-11751627585-DYeBN3Zs2yqMrdfmQQWTPUWc';
+//config.token = 'xoxb-11751627585-DYeBN3Zs2yqMrdfmQQWTPUWc';
 
-//config.token = 'xoxb-11213798960-tLVLDwm9gQVoObQv9FLNoe1C';
+config.token = 'xoxb-11213798960-tLVLDwm9gQVoObQv9FLNoe1C';
 
 config.plugins = {
+    bottest: {},
     famousquotes: {},
     "smartbot-plugin-stockquote": {},
     "smartbot-plugin-greet": {},
